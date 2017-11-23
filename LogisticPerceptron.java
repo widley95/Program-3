77aa0ae21556cacc273af8b2cb449f27dfe6d6d4
JQuit96 <julian.quitian@gmail.com>
2017-11-23 02:01:51 -0500

    Algorithm working perfectly; uncommented

--- LogisticPerceptron.java
+++ LogisticPerceptron.java
@@ -14,16 +14,16 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
     private String fileName;
 
     // The number of iterations
-    private int numEpochs;
+    private int numEpochs = 0;
 
     // Bias for all instances of perceptron
-    private int bias;
+    private double bias = 0;
 
     // Learning rate
-    private double learningRate;
+    private double learningRate = 0.0;
     
     // Logical squashing parameter
-    private double squashingParameter;
+    private double squashingParameter = 0.0;
 
     // The weights for each perceptron
     private double weights[];
@@ -42,7 +42,7 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
         this.numEpochs = Integer.parseInt(options[1]);
         this.learningRate = Double.parseDouble(options[2]);
         this.squashingParameter = Double.parseDouble(options[3]);
-        this.bias = 1;
+        this.bias = 1.0;
     }
 
    /**
@@ -85,17 +85,6 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
             
             // Go through each sample in current data sample
             for(int sampleCounter = 0; sampleCounter < dataSize; sampleCounter++){
-                // if(epochCounter == 2){
-                //     if(sampleCounter == 0){
-                //         System.out.println();
-                //         System.out.print("Weights: ");
-                //         for(int i = 0; i < weights.length; i++){
-                //             System.out.print(weights[i] + " ");
-                //         }
-                //         System.out.println();
-                //         System.exit(1);
-                //     }
-                // }
 
                 // Current instance
                 Instance currentInstance = data.instance(sampleCounter);
@@ -110,54 +99,18 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
                 // Account for bias weight
                 sum += weights[weights.length - 1];
 
-                // if(epochCounter == 2){
-                //     if(sampleCounter == 0){
-                //         for(int i = 0; i < weights.length; i++){
-                //             System.out.println();
-                //             System.out.print(weights[i] + " ");
-                //         }
-                //         System.out.println();
-                //         System.out.println(sum);
-                //         System.exit(1);
-                //     }
-                // }
-
                 // This is the result of the Perceptron. If output does not match expected output, weights will be updated.   
                 //int output = predict(currentInstance);             
                 double temp = logisticFunction(sum);
-                // if(epochCounter == 2){
-                //     if(sampleCounter == 0){
-                //         System.out.println();
-                //         System.out.println(temp);
-                //         System.exit(1);
-                //     }
-                // }
 
                 // Scale
-                temp = 2 * (temp) - 1;
-                
-                // if(epochCounter == 2){
-                //     if(sampleCounter == 0){
-                //         System.out.println();
-                //         System.out.println(temp);
-                //         System.exit(1);
-                //     }
-                // }
+                temp = 2.0 * (temp) - 1.0;
 
-                int output;
-                if(temp >= 0){
-                    output = 1;
-                }else{
-                    output = -1;
-                }
+                // Run scaled logistic output through threshold function
+                int output = thresholdFunction(temp);
                 
                 // Expected output is last value of each attribute; map +1 to distribution for class a, and -1 for class b.
                 int expectedOutput = (int)((currentInstance.value(currentInstance.attribute(numAttributes - 1)) == 0) ? 1 : -1);
-
-                // if(sampleCounter == 1){
-                //     System.out.println("Expected: " + expectedOutput + "\t Actual: " + output);
-                //     System.exit(1);
-                // }
 
                 if(output != expectedOutput){
                     System.out.print("0");
@@ -165,16 +118,16 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
 					// Increase the number of times the weight has been updated by 1
 					this.updateCount++;
 
+                    // Exponential part of derivative
                     double exponential = Math.exp(-1.0 * this.squashingParameter * sum);
+
                     // Calculate derivative
                     double fPrimeNet = (this.squashingParameter * exponential) / Math.pow(1 + exponential, 2);
 
                     // Calculate error and scale from -1 to 1
                     double error = expectedOutput - output;
-                    error = 2.0 * (error + 2.0);
-                    error = (error / 4.0) - 1.0;
 
-                    for(int m = 0; m < numAttributes; m++){
+                    for(int m = 0; m < numAttributes - 1; m++){
 
                         double deltaW = this.learningRate * error * fPrimeNet * currentInstance.value(m);
 
@@ -191,9 +144,9 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
                         //     }
                         // }
 
-                        data.attribute(m).setWeight(data.attribute(m).weight() + deltaW);
                         weights[m] = weights[m] + deltaW;
-
+                        data.attribute(m).setWeight(weights[m]);
+                        
                         // if(sampleCounter == 1){
                         //     if(m == 1){
                         //         System.out.println();
@@ -204,7 +157,8 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
                         // }
                     }
 
-                    weights[weights.length - 1] += this.learningRate * error * fPrimeNet;
+                    // Update bias weight
+                    weights[weights.length - 1] += this.learningRate * error;
 
                 }else{
                     System.out.print("1");
@@ -233,24 +187,25 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
         double sum = 0;
         // Run initial prediction function, which excludes bias
         for(int i = 0; i < instance.numAttributes() - 1; i++){
-            sum += attributes[i] * instance.attribute(i).weight();
+            // sum += attributes[i] * instance.attribute(i).weight();
+            sum += attributes[i] * this.weights[i];
         }
 
         // Account for bias by adding it's corresponding weight to the current sum
-        sum += instance.attribute(instance.numAttributes() - 1).weight() * this.bias;
+        // sum += instance.attribute(instance.numAttributes() - 1).weight() * this.bias;
+        sum += this.weights[instance.numAttributes() - 1] * this.bias;
 
-        // int output = thresholdFunction(sum);
-        // return output;
+        int output = thresholdFunction(sum);
+        return output;
 
-        double fnet = 1.0 / (1 + Math.pow(Math.E, (-1.0) * this.squashingParameter * sum));
-        return (int)fnet;
+        // double fnet = 1.0 / (1 + Math.pow(Math.E, (-1.0) * this.squashingParameter * sum));
+        // return (int)fnet;
     }
 
     // Empty concrete definition of getCapabilities() as required by the implementation
     public Capabilities getCapabilities(){
         return null;
     };
-
 
     // Empty concrete definition of classifyInstance() as required by the implementation
     public double classifyInstance(Instance data) throws Exception {
@@ -274,14 +229,15 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
 		// Prints out the required data
 		System.out.println("Source file: " + this.fileName);
 		System.out.println("Training epochs: " + this.numEpochs);
-		System.out.println("Learning rate: " + this.learningRate + "\n");
+        System.out.println("Learning rate: " + this.learningRate);
+        System.out.println("Lambda value: " + this.squashingParameter);
+        System.out.println();
         
         System.out.println("Total # weight updates = " + this.updateCount);
 		System.out.println("Final weights: \n" + finalWeights); 
         return " ";
     }
 
-    @Override
     public double[] distributionForInstance(Instance instance){
         double[] result = new double[instance.numAttributes()];
         if(predict(instance) == 1){
@@ -296,16 +252,14 @@ public class LogisticPerceptron implements weka.classifiers.Classifier{
 
     // Activation Function
     private int thresholdFunction(double n){
-        if(n >= 0)
+        if(n >= 0.0)
             return 1;
         else
             return -1;
-
-        //return (int)(1.0 / (1.0 + Math.pow(Math.E, (-1) * this.squashingParameter * n)));
     }
 
     public double logisticFunction(double sum){
-        double fnet = 1.0 / (1 + Math.pow(Math.E, (-1.0) * this.squashingParameter * sum));
+        double fnet = 1.0 / (1.0 + Math.pow(Math.E, (-1.0) * this.squashingParameter * sum));
         return fnet;
     }
 }
